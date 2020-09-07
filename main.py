ff53b6b13c5f3f590a195667a605ea3277ef0550
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2020-09-08 07:09:01 +0900

    Done
    Done

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ filetoopen=sr.AudioFile('1.wav')
 with filetoopen as source :
 	audio=Recognizer.record(source)
 text=Recognizer.recognize_google(audio)
-file1=open(r"C:\laragon\www\HOW-TO-EXTRACT-TEXT-FROM-SPEECH-speech-recognition-using-python\1.txt","a")
+file1=open(r"C:\laragon\www\HOW-TO-EXTRACT-TEXT-FROM-SPEECH-speech-recognition-using-python\1.txt","a") # your directory
 file1.writelines(text)
 file1.close()
+
